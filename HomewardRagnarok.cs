6dee9a0596baa44461a790c269aff07a9f2228ce
Kess <59174406+McDimak15@users.noreply.github.com>
2026-02-27 14:40:31 +0200

    Add files via upload

--- HomewardRagnarok.cs
+++ HomewardRagnarok.cs
@@ -10,6 +10,9 @@ namespace HomewardRagnarok
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class HomewardRagnarok : Mod
 	{
-
-	}
+        public override void HandlePacket(System.IO.BinaryReader reader, int whoAmI)
+        {
+            ModContent.GetInstance<ClamityNetHandler>().HandlePacket(reader, whoAmI);
+        }
+    }
 }
