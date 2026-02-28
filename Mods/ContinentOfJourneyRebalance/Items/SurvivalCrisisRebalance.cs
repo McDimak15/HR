5feb8fc8b1222e6e0dde30099311284b2b4ca794
Kess <59174406+McDimak15@users.noreply.github.com>
2026-02-28 13:22:42 +0200

    Added Scaling Fix for Survival Crisis

---
+++ Mods/ContinentOfJourneyRebalance/Items/SurvivalCrisisRebalance.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ModLoader;
+using System;
+using ContinentOfJourney;
+
+namespace HomewardRagnarok.ContinentOfJourneyRebalance
+{
+    public class CoJBalancePatch : ModPlayer
+    {
+        public override void PostUpdateEquips()
+        {
+            TemplatePlayer cojPlayer = Player.GetModPlayer<TemplatePlayer>();
+            if (cojPlayer.BackpackEquipped == 9)
+            {
+                float damageMult = Math.Max(Player.numMinions + 2f, 2f);
+                damageMult = (float)Math.Log2(damageMult) * 0.1f;
+                damageMult = Math.Max(0.1f, damageMult);
+                damageMult += (float)(Player.GetDamage(DamageClass.Summon).ApplyTo(1f) - 1f);
+
+                Player.GetDamage(DamageClass.Melee) /= damageMult;
+                Player.GetDamage(DamageClass.Ranged) /= damageMult;
+                Player.GetDamage(DamageClass.Magic) /= damageMult;
+
+                Player.GetDamage(DamageClass.Generic) *= damageMult;
+                Player.GetDamage(DamageClass.Summon) /= damageMult;
+            }
+        }
+    }
+}
