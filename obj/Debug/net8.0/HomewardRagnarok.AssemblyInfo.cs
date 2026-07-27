acaa9eaf8e3eff1e1a5f5a59923aeb652dac7192
Kes <dpavlyshyn2000@gmail.com>
2026-07-27 14:14:11 +0200

    temp

--- obj/Debug/net8.0/HomewardRagnarok.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomewardRagnarok.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomewardRagnarok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e62597b5f1d32592feb2b12126e3bb6ae293520d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1101840664135f1b97f8de9b80d28c0fb1c0daf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomewardRagnarok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomewardRagnarok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
